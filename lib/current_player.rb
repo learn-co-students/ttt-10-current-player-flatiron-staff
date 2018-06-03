e53cf3999ae60bf1fe403f40d0843410c8b9276b
Emily Jennings <emily.jennings@flatironschool.com>
2018-06-03 02:13:10 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,25 @@
+
 def turn_count(board)
-  taken = 0
-  board.each do |turn|
-    while turn != " "
-      taken += 1
+  counter = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      counter += 1
+    else
+      counter += 0
     end
   end
-  puts taken
+  counter
 end
 
-def current_player
+
+
+def current_player(board)
+  turns = turn_count(board)
+  if turns == 0
+    "X"
+  elsif turns == 1
+    "O"
+  elsif turns % 2
+    "X"
+  end
 end
